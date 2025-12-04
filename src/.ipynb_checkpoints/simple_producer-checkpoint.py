e291f8ddf344d119365d8c10abb493687c198321
vnaumq <vnaumovich123@gmail.com>
2025-12-05 00:11:30 +0300

    adding simple py scripts using kafka

---
+++ src/.ipynb_checkpoints/simple_producer-checkpoint.py
@@ -0,0 +1,33 @@
+from kafka import KafkaProducer
+import json
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer = lambda x: json.dumps(x).encode('utf-8')
+)
+
+print('Продюсер подключен')
+
+message = {
+    "text": "Привет, это Маша",
+    "number": 1,
+    "timestamp": "2024-12-01"
+
+}
+
+producer.send('test-topic', value=message)
+print(f"сообщение отправлено: {message}")
+
+
+for i in range(2,6):
+    new_message = {
+        "text": f"Message number: {i}"
+    }
+    producer.send('test-topic', value=new_message)
+    print(f'Сообщение отправлено: {new_message}')
+
+producer.flush()
+print("all message send")
+
+producer.close()
+print('conn close')
